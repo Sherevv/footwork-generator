4ee595b4b147a2428ba59fed82158fbaa2f63277
sherevv <sherevv@gmail.com>
2018-07-03 18:23:11 +0300

    Update version, add prerender

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,10 +1,13 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const path = require('path')
 const baseWebpackConfig = require('./webpack.config.base.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 const helpers = require('./helpers');
 
 module.exports = merge(baseWebpackConfig, {
@@ -49,7 +52,19 @@ module.exports = merge(baseWebpackConfig, {
             {from: 'src/assets/favicon.ico', flatten: true},
             {from: 'src/assets/sounds/*', to: 'assets/sounds', flatten: true},
             {from: 'src/assets/img/preview.png', to: 'assets/images', flatten: true},
-        ], {copyUnmodified: true})
+        ], {copyUnmodified: true}),
+        new PrerenderSPAPlugin({
+            // Required - The path to the webpack-outputted app to prerender.
+            staticDir: helpers.root('/dist'),
+            // Required - Routes to render.
+            // routes: [ '/', '/en/about', '/en/generator', '/en/description', '/en/exercises',
+            //     '/ru/about', '/ru/generator', '/ru/description', '/ru/exercises'],
+            routes: [ '/', '/generator', ],
+            renderer: new Renderer({
+                renderAfterTime: 10000, // Wait 5 seconds.
+            })
+        }),
+
         /*    new CompressionPlugin({
               asset: '[path].gz[query]',
               test: /\.js$/
