c5870f8c1b3d48e962c43cea292fe5a98a5f4679
sherevv <sherevv@gmail.com>
2018-07-03 17:07:15 +0300

    Add single generate button

---
+++ src/components/generate-btn/generate-btn.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
+import {SvgIconComponent} from "../../ui/svgicon";
+import './generate-btn.scss';
+
+@Component({
+    template: require('./generate-btn.vue'),
+    components: {
+        'it-svgicon': SvgIconComponent
+    }
+})
+export class GenerateBtnComponent extends Vue {
+    @Prop(String) b: string;
+    @Prop(String) n: string;
+
+    goToGenerator() {
+        this.$router.push({name: "Generator", query: {b: this.b, n: this.n}});
+    }
+}
---
+++ src/components/generate-btn/generate-btn.vue
@@ -0,0 +1,17 @@
+<span>
+    <el-button class="gogenerate-btn" type="warning" @click="goToGenerator()">
+        <it-svgicon icon="lightning" class="icon-lightning-single"></it-svgicon>
+    </el-button>
+
+    <svg style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1"
+         xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <defs>
+            <symbol id="icon-lightning" viewBox="0 0 512 512">
+                <g>
+                    <polygon points="320.557,215.066 203.181,512 246.321,215.066 	"/>
+                    <polygon points="351.806,0 234.43,296.934 160.194,296.934 203.339,0 	"/>
+                </g>
+            </symbol>
+        </defs>
+    </svg>
+</span>
