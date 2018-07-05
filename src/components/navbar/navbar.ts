188c3e556b8ea6ed1fd1a588fea1d0bbfea96937
sherevv <sherevv@gmail.com>
2018-07-05 10:43:15 +0300

    Add lang save to localeStorage

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -27,7 +27,11 @@ export class NavbarComponent extends Vue {
             }
         }else{
             if (this.$route.name) {
-                this.$router.push({name: this.$route.name, params: {lang: this.$translate.fallback}});
+                let lang = this.$ls.get('lang');
+                if (! (lang && this.$translate.langs.indexOf(lang) > -1)){
+                    lang = this.$translate.fallback;
+                }
+                this.$router.push({name: this.$route.name, params: {lang: lang}});
             }else{
                 this.$router.push({path:'/'});
             }
@@ -40,6 +44,7 @@ export class NavbarComponent extends Vue {
 
     changeLanguage(lang:string) {
         this.$translate.setLang(lang);
+        this.$ls.set('lang', lang);
         this.$router.replace({name: this.$route.name, params: {lang: lang}});
     }
 
