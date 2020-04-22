b207b14ba63010ef5eadd30137fdd6c4949f3e23
sherevv <sherevv@gmail.com>
2020-04-22 19:15:42 +0300

    Update partial include

--- src/components/app/main.ts
+++ src/components/app/main.ts
@@ -2,27 +2,26 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 import {NavbarComponent} from '../navbar';
 import {SocBtnsComponent} from '../soc-btns';
+import Footer from '../../partials/infooter.vue';
 
 @Component({
-    template: `
-    <div id="app-main">
-        <it-navbar></it-navbar>
-    
-        <main class="container">
-            <div class="container-main">
-                <div class="middle">
-                    <router-view>loading...</router-view>
-                </div>
+    template: `<div id="app-main">
+    <it-navbar></it-navbar>
+
+    <main class="container">
+        <div class="container-main">
+            <div class="middle">
+                <router-view>loading...</router-view>
             </div>
-        </main>
-    
-        <footer class="footer">
-            <div class="infooter rr">
-                ${require('../../partials/infooter.html')}
-            </div>
-        </footer>
-    </div>
-    `,
+        </div>
+    </main>
+
+    <footer class="footer">
+        <div class="infooter rr">
+           ${Footer}
+        </div>
+    </footer>
+</div>`,
     components: {
         'it-navbar': NavbarComponent,
         'it-socbtns': SocBtnsComponent
