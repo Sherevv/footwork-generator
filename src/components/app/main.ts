9d9e344d41bf20e4275c2cdbffa3a57b712f8609
sherevv <sherevv@gmail.com>
2021-08-13 13:50:53 +0300

    Migrate component to vue 3

--- src/components/app/main.ts
+++ src/components/app/main.ts
@@ -1,30 +1,29 @@
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import {NavbarComponent} from '../navbar';
-import {SocBtnsComponent} from '../soc-btns';
-import Footer from '../../partials/infooter.vue';
+import { Vue, Options } from 'vue-class-component';
+import NavbarComponent from '../navbar';
+import Footer from '@/partials/infooter.vue';
 
-@Component({
-    template: `<div id="app-main">
-    <it-navbar></it-navbar>
+@Options({
+    template: `
+      <div id="app-main">
+      <it-navbar></it-navbar>
 
-    <main class="container">
+      <main class="container">
         <div class="container-main">
-            <div class="middle">
-                <router-view>loading...</router-view>
-            </div>
+          <div class="middle">
+            <router-view/>
+          </div>
         </div>
-    </main>
+      </main>
 
-    <footer class="footer">
+      <footer class="footer">
         <div class="infooter rr">
-           ${Footer}
+          <it-footer></it-footer>
         </div>
-    </footer>
-</div>`,
+      </footer>
+      </div>`,
     components: {
         'it-navbar': NavbarComponent,
-        'it-socbtns': SocBtnsComponent
+        'it-footer': Footer
     },
 })
 export default class App extends Vue {
