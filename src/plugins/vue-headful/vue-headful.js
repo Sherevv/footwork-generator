84814cd358b9872bb775b3f2b316fd3ec4dfd42d
sherevv <sherevv@gmail.com>
2021-08-13 14:08:04 +0300

    Add vue-headful plugin

---
+++ src/plugins/vue-headful/vue-headful.js
@@ -0,0 +1,30 @@
+import headful from 'headful';
+
+const handler = props => headful(getPassedProps(props));
+
+export default {
+    template:"",
+    name: 'vue-headful',
+    props: Object.keys(headful.props),
+    watch: {
+        '$props': {
+            handler,
+            deep: true,
+            immediate: true,
+        },
+    },
+    activated() {
+        // required for keep-alive components https://vuejs.org/v2/api/#keep-alive
+        handler(this.$props);
+    },
+    render() {},
+};
+
+function getPassedProps(props) {
+    return Object.keys(props).reduce((passedProps, propKey) => {
+        if (props[propKey] !== undefined) {
+            passedProps[propKey] = props[propKey];
+        }
+        return passedProps;
+    }, {});
+}
