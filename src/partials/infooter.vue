777444e77234cda8bddbe41445ff0262611122f3
sherevv <sherevv@gmail.com>
2021-08-13 13:58:13 +0300

    Migrate component to vue 3

--- src/partials/infooter.vue
+++ src/partials/infooter.vue
@@ -1,5 +1,20 @@
-<a class="fork-me" href="https://github.com/Sherevv/footwork-generator" target="_blank" title="Fork me!"></a>
+<template>
+  <a class="fork-me" href="https://github.com/Sherevv/footwork-generator" target="_blank" rel="noopener noreferrer"
+     title="Fork me!"></a>
+  <div>Older version 2015 <a href="/old/">here</a>.</div>
+  <it-socbtns :key="$translate.current"></it-socbtns>
+  <p>Copyright &copy; 2015-{{ new Date().getFullYear() }} Footwork Generator - Designed by
+    <a href="https://it4t.ru/" target="_blank">it4t.ru</a></p>
+</template>
+<script>
+import { Options, Vue } from "vue-class-component";
+import SocialButtonsComponent from "@/components/soc-btns/soc-btns";
 
-<it-socbtns></it-socbtns>
-<p>Copyright &copy; 2015-2020 Footwork Generator - Designed by <a href="http://it4t.ru/"
-                                                                  target="_blank">it4t.ru</a></p>
+@Options({
+  components: {
+    'it-socbtns': SocialButtonsComponent
+  },
+})
+export default class Footer extends Vue {
+}
+</script>
