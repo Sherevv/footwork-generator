043ddc86d2b63471c76b4bdd8f55f6ed439f1008
sherevv <sherevv@gmail.com>
2021-08-21 20:11:14 +0300

    Add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    tabWidth: 4,
+    singleQuote: true,
+    overrides: [
+        {
+            files: '*.vue',
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
