c478ad4abdb44b40effbe0c76522a62c5997b317
sherevv <sherevv@gmail.com>
2021-08-21 17:26:51 +0300

    Update set lang url when load

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -9,49 +9,48 @@ export default class NavbarComponent extends Vue {
 
     lang = () => this.$translate.lang();
 
-    @Watch('$route.path')
-    pathChanged() {
+    created() {
         if (process.env.NODE_ENV !== 'production') {
             console.log('lang: ');
             console.log('route', this.$route);
-            console.log('query', this.$route.params);
+            console.log('lang', this.$route.params.lang);
             console.log('lang index', this.$translate.langs.indexOf(this.$route.params.lang));
         }
-        // If lang setup in url - use it, else use fallback
-        let lang = this.$route.params.lang;
-        if (lang) {
-            if (lang != this.$translate.lang() && this.$translate.langs.indexOf(lang) !== -1) {
-                this.$translate.setLang(lang.toString());
-            }
-        } else {
-            lang = this.$translate.lang();
-            if (this.$route.name) {
-                this.$router.push({
-                    name: this.$route.name,
-                    params: {lang: lang},
-                    query: this.$route.query,
-                    replace: true
-                });
-            } else {
-                this.$router.push({path: '/'});
-            }
-        }
-    }
 
-    created() {
+        // If lang setup in url - use it, else try to get from LocalStorage, otherwise use current
+        const lang = this.$route.params.lang;
+        if (lang) {
+            if (lang != this.$translate.lang()) {
+                if (this.$translate.langs.indexOf(lang) !== -1) { // change lang
+                    this.$translate.setLang(lang.toString());
+                    this.$ls.set('lang', lang);
+                } else { // get lang from LocalStorage
+                    this.trySetLangFromLocalStorage();
+                }
+            } else {
+                this.$ls.set('lang', lang);
+            }
+        } else { // get lang from LocalStorage
+            this.trySetLangFromLocalStorage();
+        }
+
         this.$translate.setTranslationModule('common', this);
+
+        if (this.$route.name) {
+            this.$router.push({
+                name: this.$route.name,
+                params: {lang: this.$translate.lang()},
+                query: this.$route.query,
+                replace: true
+            });
+        }
     }
 
     changeLanguage(lang: string) {
         this.$translate.setLang(lang);
         this.$ls.set('lang', lang);
 
-        let route = this.$route.name;
-        if (route == null) {
-            route = undefined;
-        }
-
-        this.$router.replace({name: route, params: {lang: lang}, query: this.$route.query}).catch(error => {
+        this.$router.replace({params: {lang: lang}, query: this.$route.query}).catch(error => {
             if (error.name != "NavigationDuplicated") {
                 throw error;
             }
@@ -60,5 +59,14 @@ export default class NavbarComponent extends Vue {
 
     localizePath(name: string) {
         return {name: name, params: {lang: this.$translate.lang()}};
+    }
+
+    trySetLangFromLocalStorage(): void {
+        const lang = this.$ls.get("lang");
+        if (lang && lang != this.$translate.lang() && this.$translate.langs.indexOf(lang) !== -1) { // change lang
+            this.$translate.setLang(lang.toString());
+        } else { //  use current lang
+            this.$ls.set('lang', this.$translate.lang());
+        }
     }
 }
